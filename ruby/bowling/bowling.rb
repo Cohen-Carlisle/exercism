d15fe6005f30cde82c8a4931d17bcc3d4a62e04f
Cohen Carlisle <cohen.carlisle@gmail.com>
2016-06-14 22:26:17 -0400

    ruby/bowling continue iteration 2 refactor

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -2,10 +2,10 @@ class Game
   VERSION = 1
 
   def initialize
-    @score = 0
     @frame = 1
     @roll_in_frame = 1
     @pins_remaining = 10
+    @score = 0
     @pending_scores = []
   end
 
@@ -41,16 +41,16 @@ class Game
   end
 
   def fill_ball(pins)
-    @pins_remaining = pins == 10 ? 10 : @pins_remaining - pins
+    strike?(pins) ? reset_pins : @pins_remaining -= pins
   end
 
   def strike?(pins)
-    @frame <= 10 && @roll_in_frame == 1 && pins == 10
+    @roll_in_frame == 1 && pins == 10
   end
 
   def strike
     @pending_scores << [nil, nil, nil]
-    @frame += 1
+    reset_pins
   end
 
   def continue_frame?(pins)
@@ -64,25 +64,21 @@ class Game
   end
 
   def spare?(pins)
-    @frame <= 10 && @roll_in_frame == 2 && @pins_remaining == pins
+    @roll_in_frame == 2 && @pins_remaining == pins
   end
 
   def spare
     @pending_scores << [nil, nil]
-    @frame += 1
-    @pins_remaining = 10
-    @roll_in_frame = 1
+    reset_pins
   end
 
   def open_frame?(pins)
-    @frame <= 10 && @roll_in_frame == 2 && @pins_remaining > pins
+    @roll_in_frame == 2 && @pins_remaining > pins
   end
 
   def open_frame(pins)
     @pending_scores << [nil]
-    @frame += 1
-    @pins_remaining = 10
-    @roll_in_frame = 1
+    reset_pins
   end
 
   def handle_score(pins)
@@ -98,6 +94,12 @@ class Game
     raise "Pins must have a value from 0 to 10" unless (0..10).cover?(pins)
     raise "Pin count exceeds pins on the lane" if @pins_remaining < pins
     raise "Should not be able to roll after game is over" if game_over?
+  end
+
+  def reset_pins
+    @frame += 1
+    @roll_in_frame = 1
+    @pins_remaining = 10
   end
 
   def game_over?
