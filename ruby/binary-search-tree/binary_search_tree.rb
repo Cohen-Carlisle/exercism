fc0587264f7c457e4fa39ade44464aa4531b5c41
Cohen Carlisle <cohen.carlisle@gmail.com>
2016-06-20 19:53:51 -0400

    ruby/binary_search_tree iteration 2

--- ruby/binary-search-tree/binary_search_tree.rb
+++ ruby/binary-search-tree/binary_search_tree.rb
@@ -1,26 +1,27 @@
 class Bst
+  include Enumerable
+
   VERSION = 1
 
-  attr_reader :data
-  attr_accessor :left, :right
+  attr_reader :data, :left, :right
+
   def initialize(data)
     @data = data
-    @left = nil
-    @right = nil
   end
 
   def insert(new_data)
     if new_data <= data
-      left ? left.insert(new_data) : self.left = self.class.new(new_data)
+      left ? left.insert(new_data) : @left = self.class.new(new_data)
     else
-      right ? right.insert(new_data) : self.right = self.class.new(new_data)
+      right ? right.insert(new_data) : @right = self.class.new(new_data)
     end
   end
 
   def each(&block)
     return to_enum unless block_given?
-    left && left.each(&block)
+    left&.each(&block)
     yield data
-    right && right.each(&block)
+    right&.each(&block)
+    self
   end
 end
