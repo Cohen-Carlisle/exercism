dbb49c47897d694ca9b5c56484ebae048cdaf394
Cohen Carlisle <cohen.carlisle@gmail.com>
2016-06-21 22:16:41 -0400

    ruby/crypto_square iter 1, oh god what have i done

--- ruby/crypto-square/crypto_square.rb
+++ ruby/crypto-square/crypto_square.rb
@@ -8,7 +8,7 @@ class Crypto
   end
 
   def size
-    @size ||= Math.sqrt(normalize_plaintext.size).ceil
+    @size ||= size_float.ceil
   end
 
   def plaintext_segments
@@ -19,13 +19,37 @@ class Crypto
     @ciphertext ||= preprocessed.transpose.flatten.join
   end
 
+  def normalize_ciphertext
+    out = ciphertext.scan(/.{1,#{upper_ciphertext_segment_size}}/)
+    min_size = upper_ciphertext_segment_size - 1
+    i = -1
+    loop do
+      segment_size = out[i].size
+      break if segment_size >= min_size
+      out[i].prepend(out[i-=1].slice!(segment_size-min_size..-1))
+    end
+    out.join(' ')
+  end
+
   private
 
   def preprocessed
     return @preprocessed if @preprocessed
     ary_of_chars = plaintext_segments.map { |str| str.chars }
     last_segment = ary_of_chars.last
-    last_segment.concat(Array.new(size - last_segment.size, nil))
+    last_segment.concat(Array.new(pad(last_segment.size), nil))
     @preprocessed = ary_of_chars
+  end
+
+  def size_float
+    @size_float ||= Math.sqrt(normalize_plaintext.size)
+  end
+
+  def upper_ciphertext_segment_size
+    @upper_ciphertext_segment_size ||= [size_float.floor, 2].max
+  end
+
+  def pad(last_segment_size=nil)
+    @pad ||= size - last_segment_size
   end
 end
