312415e8c1a6545cf4a7d2332334958f5ddd32df
Cohen Carlisle <cohen.carlisle@gmail.com>
2017-06-22 19:01:35 -0400

    elixir/sublist finish iter 1

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -1,9 +1,15 @@
 defmodule Sublist do
-  def compare(a, b) do
+  def compare(a, b), do: do_compare(sublist?(a, b), sublist?(b, a))
 
+  def sublist?(a, b) when length(a) < length(b) do
+    a === Enum.slice(b, 0, length(a)) || sublist?(a, tl(b))
   end
+  def sublist?(a, b) when length(a) == length(b), do: a === b
+  def sublist?(_, _), do: false
 
-  def sublist?(a, b) do
-
-  end
+  defp do_compare(ab, ba)
+  defp do_compare(true, true), do: :equal
+  defp do_compare(true, _),    do: :sublist
+  defp do_compare(_, true),    do: :superlist
+  defp do_compare(_, _),       do: :unequal
 end
