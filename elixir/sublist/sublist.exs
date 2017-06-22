b6a035008419f23a672554ca7a40581a29f6173b
Cohen Carlisle <cohen.carlisle@gmail.com>
2017-06-22 19:13:49 -0400

    elixir/sublist pass lengths

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -1,11 +1,13 @@
 defmodule Sublist do
   def compare(a, b), do: do_compare(sublist?(a, b), sublist?(b, a))
 
-  def sublist?(a, b) when length(a) < length(b) do
-    a === Enum.take(b, length(a)) || sublist?(a, tl(b))
+  def sublist?(a, b), do: do_sublist?(a, b, length(a), length(b))
+
+  defp do_sublist?(a, b, la, lb) when la < lb do
+    a === Enum.take(b, la) || do_sublist?(a, tl(b), la, lb - 1)
   end
-  def sublist?(a, b) when length(a) == length(b), do: a === b
-  def sublist?(_, _), do: false
+  defp do_sublist?(a, b, la, lb) when la == lb, do: a === b
+  defp do_sublist?(_, _, _, _),                 do: false
 
   defp do_compare(ab, ba)
   defp do_compare(true, true), do: :equal
