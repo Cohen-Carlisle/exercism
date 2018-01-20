033939abf12eee8230237c151474fb12cd0065ed
Cohen Carlisle <cohen.carlisle@gmail.com>
2018-01-20 17:28:36 -0500

    ruby/pangram restore to submission from experiment

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -2,8 +2,7 @@ class Pangram
   VERSION = 1
 
   def self.is_pangram?(str)
-    # downcased_str = str.downcase
-    # (?a..?z).all? { |char| downcased_str.include?(char) }
-    str.chars.select { |c| c =~ /\w/ }.uniq.count >= 26
+    downcased_str = str.downcase
+    (?a..?z).all? { |char| downcased_str.include?(char) }
   end
 end
