aae3918b153948bc223116ad857cce127d003747
Cohen Carlisle <cohen.carlisle@gmail.com>
2018-01-31 22:13:53 -0500

    elixir/nth-prime iter 2, tuple implementation
    it takes about 80-90 ms to find the 10,001st prime

--- elixir/nth-prime/nth_prime.exs
+++ elixir/nth-prime/nth_prime.exs
@@ -1,24 +1,30 @@
 defmodule Prime do
-  @primes_seed [3, 2]
+  @primes_seed {2, 3}
 
   @doc """
   Generates the nth prime.
   """
   @spec nth(non_neg_integer) :: non_neg_integer
   def nth(n) when n > 2, do: do_nth(n, @primes_seed)
-  def nth(n) when n in [1, 2], do: @primes_seed |> Enum.reverse() |> Enum.at(n - 1)
+  def nth(n) when n in [1, 2], do: elem(@primes_seed, n - 1)
 
-  defp do_nth(n, primes) when n > length(primes) do
-    do_nth(n, [next_prime(primes, hd(primes) + 2) | primes])
+  defp do_nth(n, primes) when n > tuple_size(primes) do
+    do_nth(n, Tuple.append(primes, next_prime(primes, last_elem(primes) + 2)))
   end
 
-  defp do_nth(n, primes) when n == length(primes), do: hd(primes)
+  defp do_nth(_n, primes), do: last_elem(primes)
 
   defp next_prime(primes, candidate) do
     (is_prime?(candidate, primes) && candidate) || next_prime(primes, candidate + 2)
   end
 
-  defp is_prime?(candidate, primes) do
-    !Enum.any?(primes, &(rem(candidate, &1) == 0))
+  defp is_prime?(candidate, primes), do: do_is_prime?(candidate, primes, 0, :math.sqrt(candidate))
+
+  defp do_is_prime?(_candidate, primes, i, lim) when elem(primes, i) > lim, do: true
+
+  defp do_is_prime?(candidate, primes, i, lim) do
+    (rem(candidate, elem(primes, i)) != 0 && do_is_prime?(candidate, primes, i + 1, lim)) || false
   end
+
+  defp last_elem(tuple), do: elem(tuple, tuple_size(tuple) - 1)
 end
