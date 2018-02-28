88699398fb6cc6de74fa991ad2b0508ab692cd95
Cohen Carlisle <cohen.carlisle@gmail.com>
2018-02-28 13:06:49 -0500

    elixir/hamming iter2 same var in clause

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -8,12 +8,10 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
-  def hamming_distance(str1, str2), do: do_hamming_distance(str1, str2, 0)
+  def hamming_distance(clist1, clist2), do: do_hamming_distance(clist1, clist2, 0)
 
-  defp do_hamming_distance([char1 | tail1], [char2 | tail2], dist) do
-    do_hamming_distance(tail1, tail2, dist + ((char1 == char2 && 0) || 1))
-  end
-
-  defp do_hamming_distance([], [], dist), do: {:ok, dist}
+  defp do_hamming_distance([c | t1], [c | t2], d), do: do_hamming_distance(t1, t2, d)
+  defp do_hamming_distance([_ | t1], [_ | t2], d), do: do_hamming_distance(t1, t2, d + 1)
+  defp do_hamming_distance([], [], d), do: {:ok, d}
   defp do_hamming_distance(_, _, _), do: {:error, "Lists must be the same length"}
 end
