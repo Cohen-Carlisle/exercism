55ba3d1faaffad6bd254d245a8c419875bf15d0f
Cohen Carlisle <cohen.carlisle@gmail.com>
2018-04-05 11:46:55 -0400

    elixir/leap iter 2, now with custom guards!

--- elixir/leap/leap.exs
+++ elixir/leap/leap.exs
@@ -1,4 +1,10 @@
+defmodule Integer.Guards do
+  defguard is_divisible(int, by) when rem(int, by) == 0
+end
+
 defmodule Year do
+  import Integer.Guards
+
   @doc """
   Returns whether 'year' is a leap year.
 
@@ -8,13 +14,9 @@ defmodule Year do
     except every year that is evenly divisible by 100
       unless the year is also evenly divisible by 400
   """
-  @spec leap_year?(non_neg_integer) :: boolean
-  def leap_year?(year) do
-    cond do
-      rem(year, 400) == 0 -> true
-      rem(year, 100) == 0 -> false
-      rem(year, 4) == 0 -> true
-      true -> false
-    end
-  end
+  @spec leap_year?(integer) :: boolean
+  def leap_year?(year) when is_divisible(year, 400), do: true
+  def leap_year?(year) when is_divisible(year, 100), do: false
+  def leap_year?(year) when is_divisible(year, 4), do: true
+  def leap_year?(year) when is_integer(year), do: false
 end
