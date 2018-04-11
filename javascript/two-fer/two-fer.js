5f3336f9eff74d016f5e2674d1ef54dc4068a982
Cohen Carlisle <cohen.carlisle@gmail.com>
2018-04-10 22:55:23 -0400

    javascript/two-fer iter 1

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,8 @@
+var TwoFer = function () {};
+
+TwoFer.prototype.twoFer = function (who) {
+  who = (typeof who === "undefined") ? "you" : who
+  return "One for " + who + ", one for me."
+};
+
+module.exports = TwoFer;
