a1ce06536a18f7c85d894dab195f2a6cfa232644
Cohen Carlisle <cohen.carlisle@gmail.com>
2018-04-11 12:53:21 -0400

    javascript/reverse-string iter 2; init with `var`

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,6 +1,6 @@
 module.exports = function (string) {
   reverse = "";
-  for(i = string.length - 1; i >= 0; i--) {
+  for(var i = string.length - 1; i >= 0; i--) {
     reverse = reverse + string[i];
   }
   return reverse;
