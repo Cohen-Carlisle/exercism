dc1dc72e8c830117f22906e597b6b41940fac5a4
Cohen Carlisle <cohen.carlisle@gmail.com>
2018-04-27 14:26:19 -0400

    elixir/list-ops iter 2, more TCO

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -1,25 +1,29 @@
 defmodule ListOps do
-  def count(list), do: countr(list, 0)
-  defp countr([], total), do: total
-  defp countr([_head | tail], total), do: countr(tail, total + 1)
+  def count(list), do: do_count(list, 0)
 
-  def reverse(list), do: reverser(list, [])
-  defp reverser([], rev_list), do: rev_list
-  defp reverser([head | tail], rev_list), do: reverser(tail, [head | rev_list])
+  defp do_count([_h | t], total), do: do_count(t, total + 1)
+  defp do_count([], total), do: total
 
-  def map([], _func), do: []
-  def map([head | tail], func), do: [func.(head) | map(tail, func)]
+  def reverse(list), do: do_reverse(list, [])
 
-  def filter([], _func), do: []
-  def filter([head | tail], func) do
-    if func.(head), do: [head | filter(tail, func)], else: filter(tail, func)
-  end
+  defp do_reverse([h | t], acc), do: do_reverse(t, [h | acc])
+  defp do_reverse([], acc), do: acc
 
+  def map(list, func), do: do_map(list, func, [])
+
+  def do_map([], _func, acc), do: reverse(acc)
+  def do_map([h | t], func, acc), do: do_map(t, func, [func.(h) | acc])
+
+  def filter(list, func), do: do_filter(list, func, [])
+
+  defp do_filter([h | t], func, acc), do: do_filter(t, func, func.(h) && [h | acc] || acc)
+  defp do_filter([], _func, acc), do: reverse(acc)
+
+  def reduce([h | t], acc, func), do: reduce(t, func.(h, acc), func)
   def reduce([], acc, _func), do: acc
-  def reduce([head | tail], acc, func), do: reduce(tail, func.(head, acc), func)
 
+  def append([h | t], list), do: [h | append(t, list)]
   def append([], list), do: list
-  def append([head | tail], list), do: [head | append(tail, list)]
 
   def concat(list), do: list |> reverse |> reduce([], &append/2)
 end
