ac329876c80eb92be24ea9d0821cbb208c79a399
Cohen Carlisle <cohencarlisle+github@protonmail.com>
2018-08-07 00:05:02 -0400

    elixir/markdown pass 1
    this commit refactors individual functions for readability.
    it does not intend to change the behavior of the code whatsoever.
    nor does it intend to change the way functions interact with each other.
    although it has some TODOs, it does not attempt to find all bugs.

--- elixir/markdown/markdown.exs
+++ elixir/markdown/markdown.exs
@@ -1,3 +1,4 @@
+# TODO: fix doctest
 defmodule Markdown do
   @doc """
     Parses a given string with Markdown syntax and returns the associated HTML for that string.
@@ -7,73 +8,86 @@ defmodule Markdown do
     iex> Markdown.parse("This is a paragraph")
     "<p>This is a paragraph</p>"
 
-    iex> Markdown.parse("#Header!\n* __Bold Item__\n* _Italic Item_")
-    "<h1>Header!</h1><ul><li><em>Bold Item</em></li><li><i>Italic Item</i></li></ul>"
+    iex> Markdown.parse("#heading!\n* __Bold Item__\n* _Italic Item_")
+    "<h1>heading!</h1><ul><li><em>Bold Item</em></li><li><i>Italic Item</i></li></ul>"
   """
   @spec parse(String.t()) :: String.t()
-  def parse(m) do
-    patch(Enum.join(Enum.map(String.split(m, "\n"), fn t -> process(t) end)))
+  def parse(markdown) do
+    markdown
+    |> String.split("\n")
+    |> Enum.map(&process_md_line/1)
+    |> Enum.join()
+    |> patch_html()
   end
 
-  defp process(t) do
-    if String.starts_with?(t, "#") || String.starts_with?(t, "*") do
-      if String.starts_with?(t, "#") do
-        enclose_with_header_tag(parse_header_md_level(t))
-      else
-        parse_list_md_level(t)
-      end
-    else
-      enclose_with_paragraph_tag(String.split(t))
-    end
-  end
-
-  defp parse_header_md_level(hwt) do
-    [h | t] = String.split(hwt)
-    {to_string(String.length(h)), Enum.join(t, " ")}
-  end
-
-  defp parse_list_md_level(l) do
-    t = String.split(String.trim_leading(l, "* "))
-    "<li>" <> join_words_with_tags(t) <> "</li>"
-  end
-
-  defp enclose_with_header_tag({hl, htl}) do
-    "<h" <> hl <> ">" <> htl <> "</h" <> hl <> ">"
-  end
-
-  defp enclose_with_paragraph_tag(t) do
-    "<p>#{join_words_with_tags(t)}</p>"
-  end
-
-  defp join_words_with_tags(t) do
-    Enum.join(Enum.map(t, fn w -> replace_md_with_tag(w) end), " ")
-  end
-
-  defp replace_md_with_tag(w) do
-    replace_suffix_md(replace_prefix_md(w))
-  end
-
-  defp replace_prefix_md(w) do
+  defp process_md_line(line) do
     cond do
-      w =~ ~r/^#{"__"}{1}/ -> String.replace(w, ~r/^#{"__"}{1}/, "<strong>", global: false)
-      w =~ ~r/^[#{"_"}{1}][^#{"_"}+]/ -> String.replace(w, ~r/_/, "<em>", global: false)
-      true -> w
+      String.starts_with?(line, "#") ->
+        line
+        |> parse_heading_level()
+        |> enclose_with_heading_tag()
+
+      String.starts_with?(line, "*") ->
+        enclose_with_list_tag(line)
+
+      true ->
+        line
+        |> String.split()
+        |> enclose_with_paragraph_tag()
     end
   end
 
-  defp replace_suffix_md(w) do
+  defp parse_heading_level(line) do
+    [heading_md | text] = String.split(line)
+    {String.length(heading_md), Enum.join(text, " ")}
+  end
+
+  defp enclose_with_list_tag(line) do
+    words = line |> String.trim_leading("* ") |> String.split()
+    "<li>" <> process_md_words(words) <> "</li>"
+  end
+
+  defp enclose_with_heading_tag({heading_level, heading_text}) do
+    "<h#{heading_level}>" <> heading_text <> "</h#{heading_level}>"
+  end
+
+  defp enclose_with_paragraph_tag(words) do
+    "<p>#{process_md_words(words)}</p>"
+  end
+
+  defp process_md_words(words) do
+    words
+    |> Enum.map(&replace_md_with_html/1)
+    |> Enum.join(" ")
+  end
+
+  defp replace_md_with_html(word) do
+    word
+    |> replace_md_prefix()
+    |> replace_md_suffix()
+  end
+
+  # TODO: fix bad regex
+  defp replace_md_prefix(word) do
     cond do
-      w =~ ~r/#{"__"}{1}$/ -> String.replace(w, ~r/#{"__"}{1}$/, "</strong>")
-      w =~ ~r/[^#{"_"}{1}]/ -> String.replace(w, ~r/_/, "</em>")
-      true -> w
+      String.starts_with?(word, "__") -> String.replace_prefix(word, "__", "<strong>")
+      word =~ ~r/^[_{1}][^_+]/ -> String.replace(word, "_", "<em>", global: false)
+      true -> word
     end
   end
 
-  defp patch(l) do
-    String.replace_suffix(
-      String.replace(l, "<li>", "<ul>" <> "<li>", global: false),
-      "</li>",
-      "</li>" <> "</ul>"
-    )
+  # TODO: fix bad regex
+  defp replace_md_suffix(word) do
+    cond do
+      String.ends_with?(word, "__") -> String.replace_suffix(word, "__", "</strong>")
+      word =~ ~r/[^_{1}]/ -> String.replace(word, "_", "</em>")
+      true -> word
+    end
+  end
+
+  defp patch_html(html) do
+    html
+    |> String.replace("<li>", "<ul><li>", global: false)
+    |> String.replace_suffix("</li>", "</li></ul>")
   end
 end
