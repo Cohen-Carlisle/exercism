c57a6f59550e7d074b6810dd59ef3e10e730ccae
Cohen Carlisle <cohencarlisle+github@protonmail.com>
2019-03-02 17:17:39 -0500

    elixir/zipper iter3 cleanup struct syntax w/ alias

--- elixir/zipper/zipper.exs
+++ elixir/zipper/zipper.exs
@@ -35,7 +35,12 @@ defmodule Zipper do
   `path` stores the path walked and associated data needed to reconstruct the tree.
   `focus` is the the current node of the tree.
   """
-  @type t :: %Zipper{path: list(PathItem.t()), focus: BinTree.t()}
+
+  alias BinTree, as: BT
+  alias Zipper, as: Z
+  alias PathItem, as: PI
+
+  @type t :: %Z{path: list(PI.t()), focus: BT.t()}
   defstruct [:path, :focus]
 
   defmodule PathItem do
@@ -48,27 +53,27 @@ defmodule Zipper do
     either `left` or `right` (but not both) must be :focus.
     :focus indicates the focus of the zipper is the child node for that subtree.
     """
-    @type t :: %PathItem{value: any, left: BinTree.t() | nil | :focus, right: BinTree.t() | nil | :focus}
+    @type t :: %PI{value: any, left: BT.t() | nil | :focus, right: BT.t() | nil | :focus}
     defstruct [:value, :left, :right]
   end
 
   @doc """
   Get a zipper focused on the root node.
   """
-  @spec from_tree(BinTree.t()) :: Zipper.t()
-  def from_tree(%BinTree{} = tree) do
-    %Zipper{path: [], focus: tree}
+  @spec from_tree(BT.t()) :: Z.t()
+  def from_tree(%BT{} = tree) do
+    %Z{path: [], focus: tree}
   end
 
   @doc """
   Get the complete tree from a zipper.
   """
-  @spec to_tree(Zipper.t()) :: BinTree.t()
-  def to_tree(%Zipper{path: [], focus: tree}) do
+  @spec to_tree(Z.t()) :: BT.t()
+  def to_tree(%Z{path: [], focus: %BT{} = tree}) do
     tree
   end
 
-  def to_tree(zipper) do
+  def to_tree(%Z{} = zipper) do
     zipper
     |> up()
     |> to_tree()
@@ -77,78 +82,90 @@ defmodule Zipper do
   @doc """
   Get the value of the focus node.
   """
-  @spec value(Zipper.t()) :: any
-  def value(%Zipper{focus: %BinTree{value: value}}) do
+  @spec value(Z.t()) :: any
+  def value(%Z{focus: %BT{value: value}}) do
     value
   end
 
   @doc """
   Get the left child of the focus node, if any.
   """
-  @spec left(Zipper.t()) :: Zipper.t() | nil
-  def left(%Zipper{focus: %BinTree{left: nil}}) do
+  @spec left(Z.t()) :: Z.t() | nil
+  def left(%Z{focus: %BT{left: nil}}) do
     nil
   end
 
-  def left(%Zipper{path: path, focus: %BinTree{value: value, left: left, right: right}}) do
-    %Zipper{path: [%PathItem{value: value, left: :focus, right: right} | path], focus: left}
+  def left(%Z{path: path, focus: %BT{value: value, left: %BT{} = left, right: right}})
+      when is_list(path) do
+    %Z{path: [%PI{value: value, left: :focus, right: right} | path], focus: left}
   end
 
   @doc """
   Get the right child of the focus node, if any.
   """
-  @spec right(Zipper.t()) :: Zipper.t() | nil
-  def right(%Zipper{focus: %BinTree{right: nil}}) do
+  @spec right(Z.t()) :: Z.t() | nil
+  def right(%Z{focus: %BT{right: nil}}) do
     nil
   end
 
-  def right(%Zipper{path: path, focus: %BinTree{value: value, left: left, right: right}}) do
-    %Zipper{path: [%PathItem{value: value, left: left, right: :focus} | path], focus: right}
+  def right(%Z{path: path, focus: %BT{value: value, left: left, right: %BT{} = right}})
+      when is_list(path) do
+    %Z{path: [%PI{value: value, left: left, right: :focus} | path], focus: right}
   end
 
   @doc """
   Get the parent of the focus node, if any.
   """
-  @spec up(Zipper.t()) :: Zipper.t()
-  def up(%Zipper{path: []}) do
+  @spec up(Z.t()) :: Z.t() | nil
+  def up(%Z{path: []}) do
     nil
   end
 
-  def up(%Zipper{path: [%PathItem{value: value, left: :focus, right: right} | tail], focus: focus}) do
-    %Zipper{path: tail, focus: %BinTree{value: value, left: focus, right: right}}
+  def up(%Z{path: [%PI{value: value, left: :focus, right: right} | tail], focus: %BT{} = focus}) do
+    %Z{path: tail, focus: %BT{value: value, left: focus, right: right}}
   end
 
-  def up(%Zipper{path: [%PathItem{value: value, left: left, right: :focus} | tail], focus: focus}) do
-    %Zipper{path: tail, focus: %BinTree{value: value, left: left, right: focus}}
+  def up(%Z{path: [%PI{value: value, left: left, right: :focus} | tail], focus: %BT{} = focus}) do
+    %Z{path: tail, focus: %BT{value: value, left: left, right: focus}}
   end
 
   @doc """
   Set the value of the focus node.
   """
-  @spec set_value(Zipper.t(), any) :: Zipper.t()
-  def set_value(%Zipper{focus: %BinTree{} = focus} = zipper, value) do
-    %Zipper{zipper | focus: %BinTree{focus | value: value}}
+  @spec set_value(Z.t(), any) :: Z.t()
+  def set_value(%Z{focus: %BT{}} = zipper, value) do
+    put_in(zipper.focus.value, value)
   end
 
   @doc """
   Replace the left child tree of the focus node.
   """
-  @spec set_left(Zipper.t(), BinTree.t()) :: Zipper.t()
-  def set_left(%Zipper{} = zipper, %BinTree{} = left), do: do_set_left(zipper, left)
-  def set_left(%Zipper{} = zipper, nil), do: do_set_left(zipper, nil)
+  @spec set_left(Z.t(), BT.t()) :: Z.t()
+  def set_left(%Z{focus: %BT{}} = zipper, %BT{} = left) do
+    do_set_left(zipper, left)
+  end
 
-  defp do_set_left(%Zipper{focus: %BinTree{} = focus} = zipper, left) do
-    %Zipper{zipper | focus: %BinTree{focus | left: left}}
+  def set_left(%Z{focus: %BT{}} = zipper, nil) do
+    do_set_left(zipper, nil)
+  end
+
+  defp do_set_left(zipper, left) do
+    put_in(zipper.focus.left, left)
   end
 
   @doc """
   Replace the right child tree of the focus node.
   """
-  @spec set_right(Zipper.t(), BinTree.t()) :: Zipper.t()
-  def set_right(%Zipper{} = zipper, %BinTree{} = right), do: do_set_right(zipper, right)
-  def set_right(%Zipper{} = zipper, nil), do: do_set_right(zipper, nil)
+  @spec set_right(Z.t(), BT.t()) :: Z.t()
+  def set_right(%Z{focus: %BT{}} = zipper, %BT{} = right) do
+    do_set_right(zipper, right)
+  end
 
-  defp do_set_right(%Zipper{focus: %BinTree{} = focus} = zipper, right) do
-    %Zipper{zipper | focus: %BinTree{focus | right: right}}
+  def set_right(%Z{focus: %BT{}} = zipper, nil) do
+    do_set_right(zipper, nil)
+  end
+
+  defp do_set_right(zipper, right) do
+    put_in(zipper.focus.right, right)
   end
 end
