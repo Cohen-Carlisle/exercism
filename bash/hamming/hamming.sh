d584ae23897b022e99681f5de7577b2ff01b658b
Cohen Carlisle <cohencarlisle+github@protonmail.com>
2019-03-05 21:08:21 -0500

    bash/hamming put main function last

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -3,16 +3,6 @@
 set -o errexit
 set -o nounset
 
-main() {
-  validate_args "$@"
-
-  local dist=0
-  for (( i = 0; i < ${#1}; i++ )); do
-    [[ ${1:$i:1} != ${2:$i:1} ]] && (( ++dist ))
-  done
-  echo $dist
-}
-
 validate_args() {
   if [[ $# -ne 2 ]]; then
     echo "Usage: hamming.sh <string1> <string2>"
@@ -21,6 +11,16 @@ validate_args() {
     echo "left and right strands must be of equal length"
     exit 1
   fi
+}
+
+main() {
+  validate_args "$@"
+
+  local dist=0
+  for (( i = 0; i < ${#1}; i++ )); do
+    [[ ${1:$i:1} != ${2:$i:1} ]] && (( ++dist ))
+  done
+  echo $dist
 }
 
 main "$@"
