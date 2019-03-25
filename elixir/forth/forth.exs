5a2e164abbb1b5dac348082ee16a4f6b94a72040
Cohen Carlisle <cohencarlisle+github@protonmail.com>
2019-03-24 23:26:44 -0400

    elixir/forth iter1

--- elixir/forth/forth.exs
+++ elixir/forth/forth.exs
@@ -1,13 +1,13 @@
 defmodule Forth do
-  defstruct [:stack]
-  @opaque evaluator :: %Forth{stack: list()}
+  defstruct [:stack, :aliases]
+  @opaque evaluator :: %Forth{stack: list(), aliases: list()}
 
   @doc """
   Create a new evaluator.
   """
   @spec new :: evaluator
   def new do
-    %Forth{stack: []}
+    %Forth{stack: [], aliases: []}
   end
 
   @doc """
@@ -16,23 +16,9 @@ defmodule Forth do
   @spec eval(evaluator, String.t()) :: evaluator
   def eval(%Forth{} = evaluator, string) when is_binary(string) do
     string
-    |> preprocess()
-    |> Enum.reduce(evaluator, &do_eval/2)
-  end
-
-  defp preprocess(string) do
-    string
-    |> String.replace(~r/[^[:graph:]]+/u, " ")
-    |> String.downcase()
-    |> String.split()
-  end
-
-  defp do_eval(string_fragment, %Forth{stack: stack} = evaluator) do
-    if String.match?(string_fragment, ~r/^[0-9]+$/) do
-      %Forth{evaluator | stack: [String.to_integer(string_fragment) | stack]}
-    else
-      do_command(string_fragment, evaluator)
-    end
+    |> preprocess_string()
+    |> process_aliases(evaluator)
+    |> do_eval()
   end
 
   @doc """
@@ -46,72 +32,125 @@ defmodule Forth do
     |> Enum.join(" ")
   end
 
-  defp do_command("+", %Forth{stack: [y, x | t]} = evaluator) do
+  defp preprocess_string(string) do
+    string
+    |> String.replace(~r/[^[:graph:]]+/u, " ")
+    |> String.downcase()
+  end
+
+  defp process_aliases(string, evaluator) do
+    new_aliases =
+      ~r/(?<=^| ): (.+?) ;(?= |$)/u
+      |> Regex.scan(string, capture: :all_but_first)
+      |> List.foldr([], fn [alias_string], aliases ->
+        [name, expansion] = String.split(alias_string, " ", parts: 2)
+
+        if String.match?(name, ~r/^[0-9]+$/) do
+          raise Forth.InvalidWord, word: name
+        end
+
+        Keyword.put(aliases, String.to_atom(name), expansion)
+      end)
+
+    updated_aliases =
+      List.foldr(evaluator.aliases, new_aliases, fn {name, expansion}, aliases ->
+        Keyword.put_new(aliases, name, expansion)
+      end)
+
+    string_without_aliases = String.replace(string, ~r/(?<=^| ): (.+?) ;(?= |$)/u, "")
+
+    expanded_string =
+      Enum.reduce(updated_aliases, string_without_aliases, fn {name, expansion}, string ->
+        String.replace(string, Atom.to_string(name), expansion)
+      end)
+
+    {expanded_string, %Forth{evaluator | aliases: updated_aliases}}
+  end
+
+  defp do_eval({string, evaluator}) do
+    string
+    |> String.split()
+    |> Enum.reduce(evaluator, &handle_command/2)
+  end
+
+  defp handle_command(command, %{stack: stack} = evaluator) do
+    if String.match?(command, ~r/^[0-9]+$/) do
+      %Forth{evaluator | stack: [String.to_integer(command) | stack]}
+    else
+      do_command(command, evaluator)
+    end
+  end
+
+  defp do_command("+", %{stack: [y, x | t]} = evaluator) do
     %Forth{evaluator | stack: [x + y | t]}
   end
 
-  defp do_command("+", %Forth{}) do
+  defp do_command("+", _evaluator) do
     raise Forth.StackUnderflow
   end
 
-  defp do_command("-", %Forth{stack: [y, x | t]} = evaluator) do
+  defp do_command("-", %{stack: [y, x | t]} = evaluator) do
     %Forth{evaluator | stack: [x - y | t]}
   end
 
-  defp do_command("-", %Forth{}) do
+  defp do_command("-", _evaluator) do
     raise Forth.StackUnderflow
   end
 
-  defp do_command("*", %Forth{stack: [y, x | t]} = evaluator) do
+  defp do_command("*", %{stack: [y, x | t]} = evaluator) do
     %Forth{evaluator | stack: [x * y | t]}
   end
 
-  defp do_command("*", %Forth{}) do
+  defp do_command("*", _evaluator) do
     raise Forth.StackUnderflow
   end
 
-  defp do_command("/", %Forth{stack: [0, _x | _t]}) do
+  defp do_command("/", %{stack: [0, _x | _t]}) do
     raise Forth.DivisionByZero
   end
 
-  defp do_command("/", %Forth{stack: [y, x | t]} = evaluator) do
+  defp do_command("/", %{stack: [y, x | t]} = evaluator) do
     %Forth{evaluator | stack: [div(x, y) | t]}
   end
 
-  defp do_command("/", %Forth{}) do
+  defp do_command("/", _evaluator) do
     raise Forth.StackUnderflow
   end
 
-  defp do_command("dup", %Forth{stack: [x | t]} = evaluator) do
+  defp do_command("dup", %{stack: [x | t]} = evaluator) do
     %Forth{evaluator | stack: [x, x | t]}
   end
 
-  defp do_command("dup", %Forth{}) do
+  defp do_command("dup", _evaluator) do
     raise Forth.StackUnderflow
   end
 
-  defp do_command("drop", %Forth{stack: [_x | t]} = evaluator) do
+  defp do_command("drop", %{stack: [_x | t]} = evaluator) do
     %Forth{evaluator | stack: t}
   end
 
-  defp do_command("drop", %Forth{}) do
+  defp do_command("drop", _evaluator) do
     raise Forth.StackUnderflow
   end
 
-  defp do_command("swap", %Forth{stack: [y, x | t]} = evaluator) do
+  defp do_command("swap", %{stack: [y, x | t]} = evaluator) do
     %Forth{evaluator | stack: [x, y | t]}
   end
 
-  defp do_command("swap", %Forth{}) do
+  defp do_command("swap", _evaluator) do
     raise Forth.StackUnderflow
   end
 
-  defp do_command("over", %Forth{stack: [_y, x | _t] = stack} = evaluator) do
+  defp do_command("over", %{stack: [_y, x | _t] = stack} = evaluator) do
     %Forth{evaluator | stack: [x | stack]}
   end
 
-  defp do_command("over", %Forth{}) do
+  defp do_command("over", _evaluator) do
     raise Forth.StackUnderflow
+  end
+
+  defp do_command(word, _evaluator) do
+    raise Forth.UnknownWord, word: word
   end
 
   defmodule StackUnderflow do
