1eff2c4999543254e35ca067a26bd263f14d2514
Cohen Carlisle <cohencarlisle+github@protonmail.com>
2019-04-19 15:40:35 -0400

    bash/leap use (( for arithmetic

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -9,11 +9,11 @@ main() {
     exit 1
   fi
 
-  if [[ $(( "$1" % 400 )) -eq 0 ]]; then
+  if (( "$1" % 400 == 0 )); then
     echo "true"
-  elif [[ $(( "$1" % 100 )) -eq 0 ]]; then
+  elif (( "$1" % 100 == 0 )); then
     echo "false"
-  elif [[ $(( "$1" % 4 )) -eq 0 ]]; then
+  elif (( "$1" % 4 == 0 )); then
     echo "true"
   else
     echo "false"
