e0eba9e63ba38586486ce3fa3efa2d3c46888259
Cohen Carlisle <cohencarlisle+github@pm.me>
2020-03-06 11:08:10 -0500

    elixir/roman-numerals use iodata as acc
    this improves performances as a new binary need not be created for each
    recursive call of `do_numerals/2`

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -3,24 +3,42 @@ defmodule RomanNumerals do
   Convert the number to a roman number.
   """
   @spec numeral(pos_integer) :: String.t()
-  def numeral(number), do: do_numeral(number, "")
+  def numeral(number), do: do_numeral(number, [])
 
-  defp do_numeral(num, roman) when num >= 1000, do: do_numeral(num - 1000, roman <> "M")
-  defp do_numeral(num, roman) when num >= 0500, do: do_numeral(num - 0500, roman <> "D")
-  defp do_numeral(num, roman) when num >= 0100, do: do_numeral(num - 0100, roman <> "C")
-  defp do_numeral(num, roman) when num >= 0050, do: do_numeral(num - 0050, roman <> "L")
-  defp do_numeral(num, roman) when num >= 0010, do: do_numeral(num - 0010, roman <> "X")
-  defp do_numeral(num, roman) when num >= 0005, do: do_numeral(num - 0005, roman <> "V")
-  defp do_numeral(num, roman) when num >= 0001, do: do_numeral(num - 0001, roman <> "I")
-  defp do_numeral(0, roman), do: to_subtractive_notation(roman)
+  defp do_numeral(num, acc) when num >= 1000, do: do_numeral(num - 1000, [?M | acc])
+  defp do_numeral(num, acc) when num >= 0500, do: do_numeral(num - 0500, [?D | acc])
+  defp do_numeral(num, acc) when num >= 0100, do: do_numeral(num - 0100, [?C | acc])
+  defp do_numeral(num, acc) when num >= 0050, do: do_numeral(num - 0050, [?L | acc])
+  defp do_numeral(num, acc) when num >= 0010, do: do_numeral(num - 0010, [?X | acc])
+  defp do_numeral(num, acc) when num >= 0005, do: do_numeral(num - 0005, [?V | acc])
+  defp do_numeral(num, acc) when num >= 0001, do: do_numeral(num - 0001, [?I | acc])
+  defp do_numeral(0, acc), do: subtractive_notation(acc)
 
-  defp to_subtractive_notation(roman) do
-    roman
-    |> String.replace("DCCCC", "CM")
-    |> String.replace("CCCC", "CD")
-    |> String.replace("LXXXX", "XC")
-    |> String.replace("XXXX", "XL")
-    |> String.replace("VIIII", "IX")
-    |> String.replace("IIII", "IV")
+  defp additive_notation(iodata) do
+    iodata
+    |> Enum.reverse()
+    |> IO.iodata_to_binary()
+  end
+
+  defp subtractive_notation(iodata) do
+    iodata
+    |> additive_notation()
+    |> do_subtractive_notation()
+  end
+
+  defp do_subtractive_notation(additive_notation) do
+    String.replace(
+      additive_notation,
+      ~w[DCCCC CCCC LXXXX XXXX VIIII IIII],
+      fn
+        "DCCCC" -> "CM"
+        "CCCC" -> "CD"
+        "LXXXX" -> "XC"
+        "XXXX" -> "XL"
+        "VIIII" -> "IX"
+        "IIII" -> "IV"
+      end,
+      global: false
+    )
   end
 end
