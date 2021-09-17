662dc67e15587791ec37d69584897abb58d26695
Cohen Carlisle <cohencarlisle+github@pm.me>
2021-09-17 19:12:03 -0400

    elixir/lasagna iter 2 format fixes

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,11 +1,21 @@
 defmodule Lasagna do
-  def expected_minutes_in_oven, do: 40
+  def expected_minutes_in_oven do
+    40
+  end
 
-  def remaining_minutes_in_oven(minutes_already_in_oven), do: expected_minutes_in_oven() - minutes_already_in_oven
+  def remaining_minutes_in_oven(minutes_already_in_oven) do
+    expected_minutes_in_oven() - minutes_already_in_oven
+  end
 
-  def preparation_time_in_minutes(layers), do: layers * 2
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
 
-  def total_time_in_minutes(layers, minutes_in_oven), do: preparation_time_in_minutes(layers) + minutes_in_oven
+  def total_time_in_minutes(layers, minutes_in_oven) do
+    preparation_time_in_minutes(layers) + minutes_in_oven
+  end
 
-  def alarm, do: "Ding!"
+  def alarm do
+    "Ding!"
+  end
 end
