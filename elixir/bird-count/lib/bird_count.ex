97abd75394af1875a35a7540c544c6e0511b4b6c
Cohen Carlisle <cohencarlisle+github@pm.me>
2021-09-24 15:21:43 -0400

    elixir/bird-count iter 2; pure recursion

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,25 +1,57 @@
 defmodule BirdCount do
-  def today(list) do
-    List.first(list)
+  def today([day | _other_days]) do
+    day
+  end
+
+  def today([]) do
+    nil
+  end
+
+  def increment_day_count([day | other_days]) do
+    [day + 1 | other_days]
   end
 
   def increment_day_count([]) do
     [1]
   end
 
-  def increment_day_count(list) do
-    [hd(list) + 1 | tl(list)]
+  def has_day_without_birds?([0 | _other_days]) do
+    true
   end
 
-  def has_day_without_birds?(list) do
-    Enum.member?(list, 0)
+  def has_day_without_birds?([_day | other_days]) do
+    has_day_without_birds?(other_days)
+  end
+
+  def has_day_without_birds?([]) do
+    false
   end
 
   def total(list) do
-    Enum.sum(list)
+    do_total(list, 0)
+  end
+
+  defp do_total([day | other_days], total) do
+    do_total(other_days, total + day)
+  end
+
+  defp do_total([], total) do
+    total
   end
 
   def busy_days(list) do
-    Enum.count(list, &(&1 >= 5))
+    do_busy_days(list, 0)
+  end
+
+  defp do_busy_days([day | other_days], count) when day >= 5 do
+    do_busy_days(other_days, count + 1)
+  end
+
+  defp do_busy_days([_day | other_days], count) do
+    do_busy_days(other_days, count)
+  end
+
+  defp do_busy_days([], count) do
+    count
   end
 end
