e3c0a8386468b191c353a856ca33f99fc68dfe71
Cohen Carlisle <cohencarlisle+github@pm.me>
2021-10-10 15:48:22 -0400

    elixir/wine-cellar iter 2; maybe_filter funcs

--- elixir/wine-cellar/lib/wine_cellar.ex
+++ elixir/wine-cellar/lib/wine_cellar.ex
@@ -10,33 +10,23 @@ defmodule WineCellar do
   def filter(cellar, color, opts \\ []) do
     cellar
     |> Keyword.get_values(color)
-    |> filter_by_year(opts[:year])
-    |> filter_by_country(opts[:country])
+    |> maybe_filter_by_year(opts[:year])
+    |> maybe_filter_by_country(opts[:country])
   end
 
-  # The functions below do not need to be modified.
-
-  defp filter_by_year(wines, year)
-  defp filter_by_year(wines, nil), do: wines
-  defp filter_by_year([], _year), do: []
-
-  defp filter_by_year([{_, year, _} = wine | tail], year) do
-    [wine | filter_by_year(tail, year)]
+  defp maybe_filter_by_year(wines, nil) do
+    wines
   end
 
-  defp filter_by_year([{_, _, _} | tail], year) do
-    filter_by_year(tail, year)
+  defp maybe_filter_by_year(wines, target_year) do
+    Enum.filter(wines, fn {_, year, _} -> year == target_year end)
   end
 
-  defp filter_by_country(wines, country)
-  defp filter_by_country(wines, nil), do: wines
-  defp filter_by_country([], _country), do: []
-
-  defp filter_by_country([{_, _, country} = wine | tail], country) do
-    [wine | filter_by_country(tail, country)]
+  defp maybe_filter_by_country(wines, nil) do
+    wines
   end
 
-  defp filter_by_country([{_, _, _} | tail], country) do
-    filter_by_country(tail, country)
+  defp maybe_filter_by_country(wines, target_country) do
+    Enum.filter(wines, fn {_, _, country} -> country == target_country end)
   end
 end
