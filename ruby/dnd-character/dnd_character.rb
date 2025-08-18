836c1310c430173576e1835c5866f3ab0df3d598
Cohen Carlisle <cohencarlisle+github@pm.me>
2025-08-18 11:17:40 -0400

    ruby/dnd_character iter 1

---
+++ ruby/dnd-character/dnd_character.rb
@@ -0,0 +1,27 @@
+class DndCharacter
+  attr_reader :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma
+
+  def self.modifier(score)
+    ((score - 10) / 2)
+  end
+
+  def initialize
+    @strength = roll_attribute
+    @dexterity = roll_attribute
+    @constitution = roll_attribute
+    @intelligence = roll_attribute
+    @wisdom = roll_attribute
+    @charisma = roll_attribute
+  end
+
+  def hitpoints
+    10 + self.class.modifier(constitution)
+  end
+
+  private
+
+  def roll_attribute
+    rolls = Array.new(4) { rand(1..6) }
+    rolls.sum - rolls.min
+  end
+end
