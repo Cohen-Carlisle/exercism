756ef55f6dd17edf6248047bdb0ccdc008f4ef15
Cohen Carlisle <cohencarlisle+github@pm.me>
2025-08-24 19:10:27 -0400

    ruby/log-line-parser iter 2, now with regex

--- ruby/log-line-parser/log_line_parser.rb
+++ ruby/log-line-parser/log_line_parser.rb
@@ -1,29 +1,17 @@
 class LogLineParser
+  attr_reader :message, :log_level
+
   def initialize(line)
-    self.line = line
-  end
-
-  def message
-    parse_line[:message]
-  end
-
-  def log_level
-    parse_line[:log_level]
+    line.rstrip.match(/\A\[(?<log_level>[[:alpha:]]+)\]:(?<message>.*)\z/) => {message:, log_level:}
+    self.message = message.lstrip
+    self.log_level = log_level.downcase
   end
 
   def reformat
-    parse_line => {message:, log_level:}
     "#{message} (#{log_level})"
   end
 
   private
 
-  attr_accessor :line
-
-  def parse_line
-    raw_log_level, raw_message = line.split(":", 2)
-    log_level = raw_log_level.tr("[]", "").downcase
-    message = raw_message.strip
-    {log_level:, message:}
-  end
+  attr_writer :message, :log_level
 end
