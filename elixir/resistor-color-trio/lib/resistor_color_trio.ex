a8c193f0455af345fd1e65a5758bea6d134f55de
Cohen Carlisle <cohencarlisle+github@pm.me>
2026-06-24 21:34:04 -0400

    elixir/resistor-color-trio iter 2

--- elixir/resistor-color-trio/lib/resistor_color_trio.ex
+++ elixir/resistor-color-trio/lib/resistor_color_trio.ex
@@ -1,21 +1,8 @@
 defmodule ResistorColorTrio do
-  @colors %{
-    black: 0,
-    brown: 1,
-    red: 2,
-    orange: 3,
-    yellow: 4,
-    green: 5,
-    blue: 6,
-    violet: 7,
-    grey: 8,
-    white: 9
-  }
-
   @doc """
   Calculate the resistance value in ohms from resistor colors
   """
-  @spec label(colors :: [atom]) :: {number, :ohms | :kiloohms | :megaohms | :gigaohms}
+  @spec label(colors :: [atom]) :: {non_neg_integer(), :ohms | :kiloohms | :megaohms | :gigaohms}
   def label(colors) do
     colors
     |> process_first_two_bands()
@@ -23,12 +10,12 @@ defmodule ResistorColorTrio do
   end
 
   defp process_first_two_bands([color1, color2, color3 | _]) do
-    n = 10 * Map.fetch!(@colors, color1) + Map.fetch!(@colors, color2)
+    n = 10 * code(color1) + code(color2)
     {n, color3}
   end
 
   defp process_third_band({n, color}) do
-    raw_ohms = n * 10 ** Map.fetch!(@colors, color)
+    raw_ohms = n * 10 ** code(color)
     apply_unit(raw_ohms)
   end
 
@@ -36,4 +23,11 @@ defmodule ResistorColorTrio do
   defp apply_unit(ohms) when ohms >= 1_000_000, do: {div(ohms, 1_000_000), :megaohms}
   defp apply_unit(ohms) when ohms >= 1_000, do: {div(ohms, 1_000), :kiloohms}
   defp apply_unit(ohms), do: {ohms, :ohms}
+
+  defp code(color) do
+    Enum.find_index(
+      ~w(black brown red orange yellow green blue violet grey white)a,
+      &(color == &1)
+    )
+  end
 end
